d64769d89d78b40e2ff80ad8e1f7ed1012d1c864
joycekang <joyce_kang@hms.harvard.edu>
2021-03-03 00:38:53 -0500

    added plotReference.R function

---
+++ R/globals.R
@@ -0,0 +1,1 @@
+utils::globalVariables(c('..level..', 'UMAP1', 'UMAP2'))
--- tests/testthat/test-fbind.R
+++
@@ -1,9 +0,0 @@
-test_that("fbind() binds factor (or character)", {
-  x <- c("a", "b")
-  x_fact <- factor(x)
-  y <- c("c", "d")
-  z <- factor(c("a", "b", "c", "d"))
-
-  expect_identical(fbind(x, y), z)
-  expect_identical(fbind(x_fact, y), z)
-})
