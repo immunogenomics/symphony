0d16197cb8f46747a244473007226b68b42350c0
joycekang <joyce_kang@hms.harvard.edu>
2021-07-11 03:16:26 -0400

    update refs

---
+++ pre-built_references/colors.R
@@ -0,0 +1,63 @@
+## Defines colors used for reference cell types plotting
+
+pbmc_colors = c("B" = "#66C2A5", 
+              "DC" = "#FC8D62",
+              "HSC" = "#8DA0CB",
+              "MK" = "#E78AC3", 
+              "Mono_CD14" = "#A6D854",
+              "Mono_CD16" = "#f2ec72",
+              "NK" = "#62AAEA", 
+              "T_CD4" = "#D1C656",
+              "T_CD8" = "#968763")
+
+pancreas_colors = c('alpha'="#ed2bb1",
+                    'beta'="#239eb3",
+                    'gamma'="#d1bfec",
+                    'delta'= "#FF6347",
+                    'stellate'="#11e38c",
+                    'immune'="#812050",
+                    'ductal'="#b2d27a",
+                    'endothelial'="#4e2da6",
+                    'acinar'="#f6bb86",
+                    'schwann'="#115d52",
+                    'epsilon'="#a1def0")
+
+group.colors = c(   'B cell'='#f2bd80',
+                    'DC precursor'='#1d6d1f',
+                    'DC1'='#8c3ba0',
+                    'DC2'='#6533ed',
+                    'Early Erythroid'='#83e3f0',
+                    'Early lymphoid/T'='#fd5917',
+                    'Endothelial cell'='#4f8c9d',
+                    'Fibroblast'='#eb1fcb',
+                    'Hepatocyte'='#f5cdaf',
+                    'HSC_MPP'='#9698dc',
+                    'ILC precursor'='#20f53d',
+                    'Kupffer Cell'='#f283e3',
+                    'Late Erythroid'='#ffb2be',
+                    'Mast cell'='#f3d426',
+                    'Megakaryocyte'='#5ebf72',
+                    'MEMP'='#a67649',
+                    'Mid Erythroid'='#2f5bb1',
+                    'Mono-Mac'='#90a479',
+                    'Monocyte'='#f6932e',
+                    'Monocyte precursor'='#d59e9a',
+                    'Neut-myeloid prog.'='#caf243',
+                    'NK'='#38b5fc',
+                    'pDC precursor'='#c82565',
+                    'Pre pro B cell'='#d6061a',
+                    'pre-B cell'='#e36f6f',
+                    'pro-B cell'='#1dfee1',
+                    'VCAM1+ EI macro.'='#506356')
+
+# Assorted colors
+assort.colors = c(brewer.pal(name="Set1", n = 8), brewer.pal(name="Set2", n = 8),
+             brewer.pal(name='Set3', n = 12),
+             brewer.pal(name="Dark2", n = 8),
+             brewer.pal(name='Accent', n = 7), 
+             brewer.pal(name="Paired", n = 12),
+             brewer.pal(name='Greens', n=4))
+
+tms.colors = c(brewer.pal(name="Dark2", n = 8), brewer.pal(name="Paired", n = 12),
+            brewer.pal(name="Set1", n = 9), brewer.pal(name="Set2", n = 8),
+            brewer.pal(name='Accent', n = 7), brewer.pal(name='Set3', n = 12))
