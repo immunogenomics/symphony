c2aff82488a4ed06dbbab2a9b5a50fd582731cab
joycekang <joyce_kang@hms.harvard.edu>
2021-07-26 20:09:42 -0400

    added pbmcs .Rmd tutorial

--- R/data.R
+++ R/data.R
@@ -1,13 +1,13 @@
 #' Log(CP10k+1) normalized counts matrix (genes by cells) for 10x PBMCs dataset for vignette.
 #' 
-#' @format: Sparse matrix (dgCMatrix): dimensions 1,764 genes by 1,500 cells
+#' @format: Sparse matrix (dgCMatrix): dimensions 1,764 genes by 1,200 cells
 #' 
 #' @source \url{support.10xgenomics.com/single-cell-gene-expression/datasets}
 "pbmcs_exprs_small"
 
 #' Metadata for 10x PBMCs dataset for vignette.
 #' 
-#' @format: A data frame with 1,500 cells and 7 metadata fields. 
+#' @format: A data frame with 1,200 cells and 7 metadata fields. 
 #' \describe{
 #'   \item{cell_id}{unique cell ID}
 #'   \item{donor}{dataset (3pv1, 3pv2, or 5p)}
